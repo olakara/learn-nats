1b00b70d4e836c5adbc8a66dc3563482f5350acc
Abdel Raoof <olakara@gmail.com>
2026-07-02 12:58:24 +0400

    Add Timestamp field to Transaction struct and update NewTransaction function

--- transactions/maker/transaction/transaction.go
+++ transactions/maker/transaction/transaction.go
@@ -1,11 +1,14 @@
 package transaction
 
+import "time"
+
 type Transaction struct {
-	Id       string  `json:"id"`
-	Amount   float64 `json:"amount"`
-	PersonId string  `json:"personId"`
+	Id        string    `json:"id"`
+	Amount    float64   `json:"amount"`
+	PersonId  string    `json:"personId"`
+	Timestamp time.Time `json:"timestamp"`
 }
 
 func NewTransaction(id string, amount float64, personId string) Transaction {
-	return Transaction{Id: id, Amount: amount, PersonId: personId}
+	return Transaction{Id: id, Amount: amount, PersonId: personId, Timestamp: time.Now()}
 }
